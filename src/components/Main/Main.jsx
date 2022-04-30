3084fa4f59155d85d2850c87ace929a7449efda0
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-05-01 00:25:05 +0300

    add eslint & prop-types fix code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import style from './Main.module.css';
 import Layout from '../Layout';
 
-export const Main = () => {
-  return (
-    <main className={style.main}>
-      <Layout>мейн</Layout>
-    </main>
-  )
-}
+export const Main = () => (
+  <main className={style.main}>
+    <Layout>мейн</Layout>
+  </main>
+);
