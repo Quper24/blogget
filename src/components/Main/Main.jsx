fc3258eb3d66b1473d2c835693830294662c683d
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-05-04 02:08:57 +0300

    lesson04

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import style from './Main.module.css';
 import Layout from '../Layout';
+import Tabs from './Tabs';
+import List from './List';
+import React from 'react';
 
 export const Main = () => (
   <main className={style.main}>
-    <Layout>мейн</Layout>
+    <Layout>
+      <Tabs />
+      <List />
+    </Layout>
   </main>
 );
