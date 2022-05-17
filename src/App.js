506cf063942f706f9bec60650bc8ecd308945cb6
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-05-18 01:15:29 +0300

    part 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import { AuthContextProvider } from './context/authContext';
-import { tokenContext } from './context/tokenContext';
-import { useToken } from './hooks/useToken';
+import { TokenContextProvider } from './context/tokenContext';
 
-function App() {
-  const [token, delToken] = useToken('');
-
-  return (
-    <tokenContext.Provider value={{ token, delToken }}>
-      <AuthContextProvider>
-        <Header />
-        <Main />
-      </AuthContextProvider>
-    </tokenContext.Provider>
-  );
-}
+const App = () => (
+  <TokenContextProvider>
+    <AuthContextProvider>
+      <Header />
+      <Main />
+    </AuthContextProvider>
+  </TokenContextProvider>
+);
 
 export default App;
