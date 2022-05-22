c8b2d578c99ee1dbddd5f45761bd37fa95becbda
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-05-22 20:06:42 +0300

    video ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import { Provider } from 'react-redux';
 import { AuthContextProvider } from './context/authContext';
 import { PostContextProvider } from './context/postContext';
 import { TokenContextProvider } from './context/tokenContext';
+import { store } from './store';
 
 const App = () => (
-  <TokenContextProvider>
-    <AuthContextProvider>
-      <PostContextProvider>
-        <Header />
-        <Main />
-      </PostContextProvider>
-    </AuthContextProvider>
-  </TokenContextProvider>
+  <Provider store={store}>
+    <TokenContextProvider>
+      <AuthContextProvider>
+        <PostContextProvider>
+          <Header />
+          <Main />
+        </PostContextProvider>
+      </AuthContextProvider>
+    </TokenContextProvider>
+  </Provider>
 );
 
 export default App;
--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,13 +1,22 @@
 import style from './FormComment.module.css';
 import { authContext } from '../../../context/authContext';
 import { Text } from '../../../UI/Text';
-import { useContext, useRef } from 'react';
+import { useContext } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateComment } from '../../../store';
+
 export const FormComment = () => {
+  const value = useSelector(state => state.comment);
+  const dispatch = useDispatch();
   const { auth } = useContext(authContext);
-  const refTextarea = useRef(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(refTextarea.current.value);
+    console.log(value);
+  };
+
+  const handleChange = (e) => {
+    dispatch(updateComment(e.target.value));
   };
 
   return (
@@ -15,7 +24,11 @@ export const FormComment = () => {
       <Text As='h3' size={14} tsize={18}>
         {auth.name}
       </Text>
-      <textarea className={style.textarea} ref={refTextarea} />
+      <textarea
+        className={style.textarea}
+        value={value}
+        onChange={handleChange}
+      />
       <button className={style.btn}>Отправить</button>
     </form>
   );
