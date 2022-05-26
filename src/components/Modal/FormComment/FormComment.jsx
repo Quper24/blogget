f572a0425236f1c673b9fe4cf6416a84a5422ec8
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-05-26 04:15:08 +0300

    fix

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateComment } from '../../../store/commentReducer';
 
 export const FormComment = () => {
-  const value = useSelector(state => state.comment);
+  const value = useSelector(state => state.comment.comment);
   const dispatch = useDispatch();
   const { auth } = useContext(authContext);
 
