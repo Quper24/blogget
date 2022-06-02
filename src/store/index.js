1ecca22eb9545bc8e47d5305c952ce9b95c25041
ЬфлMaksim Leskin <maksim.leskin@methed.ru>
2022-06-02 19:46:07 +0300

    saga ready

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { tokenMiddleware, tokenReducer } from './token/tokenReducer';
 import { commentReducer } from './comment/commentReducer';
-
 import { authReducer } from './auth/authReducer';
 import postsReducer from './posts/postSlice';
 import commentsReducer from './comments/commentsSlice';
+import { searchReducer } from './search/searchReducer';
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
@@ -13,7 +17,10 @@ export const store = configureStore({
     auth: authReducer,
     posts: postsReducer,
     comments: commentsReducer,
+    search: searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tokenMiddleware),
+    getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
